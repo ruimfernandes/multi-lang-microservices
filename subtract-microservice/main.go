5072db59e12e04404b4fa726082a0f43a2528800
Rui Fernandes <ruifernandes@sioslife.com>
2018-12-20 15:13:20 +0000

    Improve subtract service GET header

--- subtract-microservice/main.go
+++ subtract-microservice/main.go
@@ -23,6 +23,7 @@ const PORT int = 8081
 
 func endpointDescription(w http.ResponseWriter, r *http.Request) {
 	endpointDesc := EndpointDescription{Description: "This is a Go service that makes subtractions. As payload receives data like the body below.", Body: Calculation{Value1: 6, Value2: 2}}
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(endpointDesc)
 }
 
@@ -40,7 +41,7 @@ func subtract(w http.ResponseWriter, r *http.Request) {
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Go service home page endpoint hit")
+	fmt.Fprint(w, "Subtract (Go) service home page endpoint hit")
 }
 
 func handleRequest() {
